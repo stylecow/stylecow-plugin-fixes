20d047f149a204f2c9d5976dbdfc5ada037cdc7b
oscarotero <oom@oscarotero.com>
2015-01-07 21:53:12 +0100

    Included the 'initial' plugin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function (stylecow) {
 	require('./src/clip')(stylecow);
 	require('./src/column-break')(stylecow);
 	require('./src/float')(stylecow);
+	require('./src/initial')(stylecow);
 	require('./src/inline-block')(stylecow);
 	require('./src/min-height')(stylecow);
 	require('./src/opacity')(stylecow);
