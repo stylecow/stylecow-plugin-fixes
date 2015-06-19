f4bdb60b1c53b73b6db1b822b7fd9be13bb4cde0
oscarotero <oom@oscarotero.com>
2015-06-19 15:38:30 +0200

    changed dependencies

--- src/opacity.js
+++ src/opacity.js
@@ -13,8 +13,37 @@ module.exports = function (stylecow) {
 			var block = declaration.getParent('Block');
 
 			if (block) {
-				stylecow.utils.addMsFilter(block, 'alpha(opacity=' + (parseFloat(declaration[0], 10) * 100) + ')');
+				addMsFilter(block, 'alpha(opacity=' + (parseFloat(declaration[0], 10) * 100) + ')');
 			}
 		}
 	});
+
+	function addMsFilter (block, filter) {
+		var declaration = block.getChild({
+				type: 'Declaration',
+				name: 'filter',
+				vendor: 'ms'
+			});
+
+		if (!declaration) {
+			return block.push(stylecow.parse('-ms-filter: ' + filter, 'Declaration', 'createMsFilter'));
+		}
+
+		if (declaration.is({string: '-ms-filter: none;'})) {
+			return declaration
+				.get({
+					type: 'Keyword',
+					name: 'none'
+				})
+				.replaceWith((new stylecow.String()).setName(filter));
+		}
+
+		var string = declaration.get('String');
+
+		if (string.name) {
+			string.name += ',' + filter;
+		} else {
+			string.name = filter;
+		}
+	}
 };
